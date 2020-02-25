55f9333634d2f09f2ec2e52dd6376ee11a8a51d1
apple <apple@apples-MacBook-Pro-2.local>
2020-02-25 15:12:55 -0800

    setting up login page, routes, edited password on server

---
+++ friends/src/components/Login.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class Login extends React.Component {
+    state={
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        axios
+        .post('http://localhost:5000/api/friends', this.state.credentials)
+        .then(res => console.log(res))
+        .catch(err => console.log('error at axios', err));
+    };
+
+    render(){
+    return (
+        <>
+        <h2>Login Form</h2>
+            <form onSubmit={this.login}>
+                <input
+                    name='username'
+                    placeholder='Login'
+                    type='text'
+                    value={this.state.credentials.username}
+                    onChange={this.handleChange}
+                /><br/>
+                <input 
+                    name='password'
+                    placeholder='Password'
+                    type='password'
+                    value={this.state.credentials.password}
+                    onChange={this.handleChange}
+                />
+                <br/>
+                <button>Log In</button>
+                </form>        
+        </>
+    )
+    }
+};
+
+export default Login;
